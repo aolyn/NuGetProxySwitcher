bf9f82c28614f74350e6fef8c16cc584ffc48944
Chris <chrishwang@live.cn>
2016-11-04 21:53:14 +0800

    change AssemblyName
    init

---
+++ src/Aolyn.NuGetTools.ProxySwitcher/Aolyn.NuGetTools.ProxySwitcher/FormProxySwitcher.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Aolyn.NuGetTools.ProxySwitcher
+{
+    partial class FormProxySwitch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.chkUseProxy = new System.Windows.Forms.CheckBox();
+			this.txtProxyAddress = new System.Windows.Forms.TextBox();
+			this.lsbProxyList = new System.Windows.Forms.ListBox();
+			this.txtConfigFile = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// chkUseProxy
+			// 
+			this.chkUseProxy.AutoSize = true;
+			this.chkUseProxy.Location = new System.Drawing.Point(8, 10);
+			this.chkUseProxy.Name = "chkUseProxy";
+			this.chkUseProxy.Size = new System.Drawing.Size(71, 17);
+			this.chkUseProxy.TabIndex = 1;
+			this.chkUseProxy.Text = "UseProxy";
+			this.chkUseProxy.UseVisualStyleBackColor = true;
+			this.chkUseProxy.Click += new System.EventHandler(this.chkUseProxy_Click);
+			// 
+			// txtProxyAddress
+			// 
+			this.txtProxyAddress.Location = new System.Drawing.Point(85, 8);
+			this.txtProxyAddress.Name = "txtProxyAddress";
+			this.txtProxyAddress.Size = new System.Drawing.Size(297, 20);
+			this.txtProxyAddress.TabIndex = 2;
+			this.txtProxyAddress.Text = "http://localhost:1080";
+			// 
+			// lsbProxyList
+			// 
+			this.lsbProxyList.FormattingEnabled = true;
+			this.lsbProxyList.Location = new System.Drawing.Point(8, 59);
+			this.lsbProxyList.Name = "lsbProxyList";
+			this.lsbProxyList.Size = new System.Drawing.Size(374, 212);
+			this.lsbProxyList.TabIndex = 3;
+			this.lsbProxyList.DoubleClick += new System.EventHandler(this.lsbProxyList_DoubleClick);
+			// 
+			// txtConfigFile
+			// 
+			this.txtConfigFile.Location = new System.Drawing.Point(8, 33);
+			this.txtConfigFile.Name = "txtConfigFile";
+			this.txtConfigFile.Size = new System.Drawing.Size(374, 20);
+			this.txtConfigFile.TabIndex = 2;
+			// 
+			// FormProxySwitch
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(390, 284);
+			this.Controls.Add(this.lsbProxyList);
+			this.Controls.Add(this.txtConfigFile);
+			this.Controls.Add(this.txtProxyAddress);
+			this.Controls.Add(this.chkUseProxy);
+			this.Name = "FormProxySwitch";
+			this.Text = "NuGet Proxy Switcher";
+			this.Load += new System.EventHandler(this.FormProxySwitch_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox chkUseProxy;
+        private System.Windows.Forms.TextBox txtProxyAddress;
+        private System.Windows.Forms.ListBox lsbProxyList;
+        private System.Windows.Forms.TextBox txtConfigFile;
+    }
+}
+
---
+++ src/Aolyn.NuGetTools.ProxySwitcher/Aolyn.NuGetTools.ProxySwitcher/FormProxySwitcher.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Aolyn.NuGetTools.ProxySwitcher
+{
+	public partial class FormProxySwitch : Form
+	{
+		public FormProxySwitch()
+		{
+			InitializeComponent();
+		}
+
+		private void lsbProxyList_DoubleClick(object sender, EventArgs e)
+		{
+			var proxyAddr = lsbProxyList.SelectedItem as string;
+
+			SetProxy(proxyAddr);
+
+			chkUseProxy.Checked = true;
+			txtProxyAddress.Text = proxyAddr;
+		}
+
+		private static void SetProxy(string proxyAddr)
+		{
+			var configFile = GetConfigPath();
+			var doc = new XmlDocument();
+			doc.Load(configFile);
+			if (doc.DocumentElement == null)
+				return;
+
+			var configNode = doc.DocumentElement["config"];
+			if (configNode == null)
+			{
+				configNode = doc.CreateElement("config");
+				doc.DocumentElement.AppendChild(configNode);
+			}
+
+			var httpProxyNode = configNode.SelectSingleNode("add[@key='http_proxy']");
+			if (httpProxyNode == null)
+			{
+				httpProxyNode = doc.CreateElement("add");
+				configNode.AppendChild(httpProxyNode);
+
+				var keyAttr = doc.CreateAttribute("key");
+				keyAttr.Value = "http_proxy";
+				httpProxyNode.Attributes.Append(keyAttr);
+			}
+
+			if (httpProxyNode.Attributes["value"] == null)
+			{
+				var valueAttr = doc.CreateAttribute("value");
+				httpProxyNode.Attributes.Append(valueAttr);
+				valueAttr.Value = proxyAddr;
+			}
+			else
+			{
+				httpProxyNode.Attributes["value"].Value = proxyAddr;
+			}
+
+			doc.Save(configFile);
+		}
+
+		private static void RemoveProxy()
+		{
+			var configFile = GetConfigPath();
+			var doc = new XmlDocument();
+			doc.Load(configFile);
+
+			var httpProxyNode = doc.DocumentElement.SelectSingleNode("config/add[@key='http_proxy']");
+			httpProxyNode?.ParentNode.RemoveChild(httpProxyNode);
+
+			doc.Save(configFile);
+		}
+
+		private void FormProxySwitch_Load(object sender, EventArgs e)
+		{
+			var file = "ProxyList.xml";
+
+			using (var fs = File.OpenRead(file))
+			{
+				try
+				{
+					var ser = new XmlSerializer(typeof(string[]));
+					var proxyList = ser.Deserialize(fs) as string[];
+					if (proxyList != null)
+					{
+						lsbProxyList.Items.AddRange(proxyList);
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(@"Read ProxyList.xml error: " + ex);
+				}
+			}
+
+			//var file = "ProxyList.xml";
+			//var list = new[]
+			//{
+			//	"a",
+			//	"b"
+			//};
+
+			//var ser = new XmlSerializer(typeof(string[]));
+			//var ms = new MemoryStream();
+			//ser.Serialize(ms, list);
+			//var str = Encoding.UTF8.GetString(ms.ToArray());
+
+			var configFile = GetConfigPath();
+			if (!File.Exists(configFile))
+			{
+				MessageBox.Show(configFile + " not exist");
+				Close();
+			}
+			txtConfigFile.Text = configFile;
+
+			var doc = new XmlDocument();
+			doc.Load(configFile);
+			var configNode = doc.DocumentElement["config"];
+			var httpProxyNode = configNode?.SelectSingleNode("add[@key='http_proxy']");
+			if (httpProxyNode?.Attributes["value"] != null)
+			{
+				lsbProxyList.Items.Add(httpProxyNode.Attributes["value"].Value);
+				txtProxyAddress.Text = httpProxyNode.Attributes["value"].Value;
+				chkUseProxy.Checked = true;
+			}
+
+		}
+
+		private static string GetConfigPath()
+		{
+			var path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+			var configFile = Path.Combine(path, @"AppData\Roaming\NuGet\NuGet.Config");
+			return configFile;
+		}
+
+		private void chkUseProxy_CheckedChanged(object sender, EventArgs e)
+		{
+		}
+
+		private void chkUseProxy_Click(object sender, EventArgs e)
+		{
+			if (chkUseProxy.Checked == false)
+			{
+				RemoveProxy();
+				chkUseProxy.Checked = false;
+			}
+			else
+			{
+				SetProxy(txtProxyAddress.Text);
+			}
+
+		}
+	}
+}
